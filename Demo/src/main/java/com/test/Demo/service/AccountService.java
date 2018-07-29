87a847ba09cba1ba031bb99d581d25e0e9582321
Win <Win@192.168.0.5>
2018-07-30 00:59:37 +0530

    Service Interfaces

--- Demo/src/main/java/com/test/Demo/service/AccountService.java
+++ Demo/src/main/java/com/test/Demo/service/AccountService.java
@@ -1,71 +1,9 @@
 package com.test.Demo.service;
 
-import java.util.List;
+import com.test.Demo.model.BankLogin;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.test.Demo.model.Account;
-import com.test.Demo.repository.BankDummyRepository;
-
-@Service
-public class AccountService {
-
-	@Autowired
-	private BankDummyRepository repository;
-
-	/**
-	 * Logging into the bank.
-	 * 
-	 * @param bankName
-	 * @return
-	 */
-	public Object loginToBank(String bankName) {
-		boolean isLoggedIn = repository.isAuthenticated();
-
-		if (!isLoggedIn) {
-			return repository.prepareErrorMessage("You have not authorized to log into " + bankName);
-		}
-
-		return "";
-	}
-
-	/**
-	 * Retrieving the list of accounts of the user from the specified bank.
-	 * 
-	 * @param bankName
-	 * @return
-	 */
-	public Object getAccounts(String bankName) {
-		boolean isLoggedIn = repository.isBankAuthenticated();
-
-		if (!isLoggedIn) {
-			return repository.prepareErrorMessage("You have not logged into " + bankName);
-		}
-
-		// Retrieving the account details of the user.
-		List<Account> accountList = repository.getAccountDetails(bankName);
-		return accountList;
-	}
-
-	/**
-	 * Retrieving the transaction details of the specified account number.
-	 * 
-	 * @param accountNumber
-	 * @return
-	 */
-	public Object getAccountTransactionDetails(String accountNumber) {
-
-		boolean isLoggedIn = repository.isBankAuthenticated();
-
-		if (!isLoggedIn) {
-			return repository.prepareErrorMessage("You have not logged into bank.");
-		}
-		
-		long accountNum = Long.valueOf(accountNumber);
-		// Retrieving the account details of the user.
-		Account account = repository.getAccountTransactionDetails(accountNum);
-		return account;
-	}
-
+public interface AccountService {
+	public Object loginToBank(String bankName, BankLogin bankLogin);
+	public Object getAccounts(String bankName);
+	public Object getAccountTransactionDetails(String accountNumber);
 }
--- Demo/src/main/java/com/test/Demo/service/LoginService.java
+++ Demo/src/main/java/com/test/Demo/service/LoginService.java
@@ -1,32 +1,9 @@
 package com.test.Demo.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.test.Demo.model.RegistrationFailure;
 import com.test.Demo.model.UserRegistration;
-import com.test.Demo.repository.BankDummyRepository;
 
-@Service
-public class LoginService {
-
-	@Autowired
-	private BankDummyRepository repository;
-	
-	public Object register(UserRegistration userRegistration) {
-		return repository.register(userRegistration);
-	}
-	
-	public Object login(UserRegistration userRegistration) {
-		
-		boolean isAuthenticated = repository.login(userRegistration);
-		
-		if(!isAuthenticated) {
-			RegistrationFailure registrationFailure = new RegistrationFailure("Login Failed","Yes");
-			return registrationFailure;
-		}
-		
-		return "";
-	}
+public interface LoginService {
+	public Object register(UserRegistration userRegistration);
+	public Object login(UserRegistration userRegistration);
 	
 }
